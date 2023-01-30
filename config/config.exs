d83e9f8f0c22a009706ecb7cf03787f1347018e0
5HT <maxim@synrc.com>
2023-01-30 15:11:30 +0200

    iex -S mix

---
+++ config/config.exs
@@ -0,0 +1,35 @@
+use Mix.Config
+
+config :bpe,
+  ttl: :infinity,
+  ping_discipline: :undefined,
+  shutdown_timeout: 20000,
+  timeout: 30000,
+  procmodules: [
+    :bpe_account,
+  ],
+  logger_level: :error,
+  logger: [
+    {:handler, :synrc, :logger_std_h,
+     %{
+       level: :debug,
+       id: :synrc,
+       max_size: 2000,
+       module: :logger_std_h,
+       config: %{type: :file, file: 'erp.log'},
+       formatter:
+         {:logger_formatter,
+          %{
+            template: [:time, ' ', :pid, ' ', :module, ' ', :msg, '\n'],
+            single_line: true
+          }}
+     }}
+  ]
+
+config :kvs,
+  dba: :kvs_rocks,
+  dba_st: :kvs_st,
+  schema: [:kvs, :kvs_stream, :schema]
+
+config :schema,
+  boot: []
