2731197203f60a8efefa1e149b76430d8ce2e6f1
5HT <maxim@synrc.com>
2023-02-04 05:13:18 +0200

    more fields and stages

--- lib/fields.ex
+++ lib/fields.ex
@@ -5,10 +5,17 @@ defmodule Fields do
       :lists.flatten(:lists.map(fn char -> Fields.dispatch(char) end, users))
   end
 
+  def dispatch(65), do: :approvers
   def dispatch(68), do: :to
+  def dispatch(69), do: :executor
+  def dispatch(73), do: :initiator
   def dispatch(77), do: :modified_by
+  def dispatch(78), do: :target_notify
   def dispatch(82), do: :registered_by
+  def dispatch(83), do: :signatory
   def dispatch(84), do: :target
+  def dispatch(66), do: :bizTask_initiator
   def dispatch(_ ), do: []
+
 
 end
--- lib/stages.ex
+++ lib/stages.ex
@@ -2,13 +2,26 @@ defmodule Stages do
 
   def dis('Cr'),   do: 'Created'
   def dis('Det'),  do: 'Determination'
-  def dis('C'),    do: 'Confirmation'
+  def dis('C'),    do: 'Certification'
+  def dis('S'),    do: 'SignatureImposition'
+  def dis('Se'),   do: 'Send'
+  def dis('S2'),   do: 'Sending'
+  def dis('D'),    do: 'Development'
+  def dis('Ag'),   do: 'Agreement'
+  def dis('Cv'),   do: 'Convert'
+  def dis('E'),    do: 'Execution'
   def dis('A'),    do: 'Archive'
   def dis('gwC'),  do: 'gwConfirmation'
+  def dis('gwS'),  do: 'gwSigned'
+  def dis('gwA'),  do: 'gwAgreed'
+  def dis('gwR'),  do: 'gwRejected'
+  def dis('gwV'),  do: 'gwConverted'
   def dis('InC'),  do: 'InitialConsideration'
   def dis('I'),    do: 'Implementation'
+  def dis('IC'),   do: 'ImplementationControl'
   def dis('R'),    do: 'Registration'
   def dis('gwND'), do: 'gwNeedsDetermination'
+  def dis('gwNC'), do: 'gwNeedsCertification'
   def dis(x),      do: x
 
 end
